215f409512453dc5183a9d80e61eb3b893d713f9
CezaryKepa <max23273@gmail.com>
2020-11-01 16:20:58 +0100

    added more sorting algorithms

--- utility.js
+++ utility.js
@@ -1,14 +1,14 @@
 export async function highlightAndSleepBubble(prev, next){
     prev.style.background = '#e7de3c';
     next.style.background = '#e7de3c';
-    await sleep(1);
+    await sleep(15);
     prev.style.background = '#E73C4E';
     next.style.background = '#E73C4E';
 }
 export async function highlightAndSleepInsertion(prev, next){
     prev.style.background = '#e7de3c';
     next.style.background = '#4de73c';
-    await sleep(0.2);
+    await sleep(1);
     prev.style.background = '#3c61e7';
     next.style.background = '#3c61e7';
 }
@@ -16,6 +16,13 @@ export async function highlightAndSleepSelection(highlighted) {
     highlighted.style.background = '#e7de3c';
     await sleep(1);
     highlighted.style.background = '#E73C4E';
+}
+export async function highlightAndSleepHeap(prev, next){
+    prev.style.background = '#e7de3c';
+    next.style.background = '#4de73c';
+    await sleep(50);
+    prev.style.background = '#E73C4E';
+    next.style.background = '#E73C4E';
 }
 export function swap(element1, element2) {
     const el1Height = element1.style.height;
